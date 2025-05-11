c3c0fb24be4304d16ee7805f6412424018732f32
Shadab Ali <sidshaab97@gmail.com>
2025-05-11 20:38:50 +0530

    add  __dirname

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,6 +15,12 @@ import { defineConfig, devices } from "@playwright/test";
 import dotenv from "dotenv";
 import path from "path";
 
+import { fileURLToPath } from "url";
+
+// Define __dirname manually
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Read from ".env" file.
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
