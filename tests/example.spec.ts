a5e8354619cdd51910e8171ee7ebed0b70d77716
Shadab Ali <sidshaab97@gmail.com>
2025-05-11 15:11:16 +0530

    add all

---
+++ tests/example.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  // ad automated test for playwright
+
+  // Go to the Playwright homepage
+
+  await page.goto("https://playwright.dev/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
+});
